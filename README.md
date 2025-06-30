4c65a10dd996589c2b4cf2b9515eaff29dc7ba40
jimstir <jimmydebe0@gmail.com>
2025-06-29 22:21:59 -0400

    Updates but not finish in time for hack :(

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-## OpenTreasury
+# OpenTreasury
 
+## Components
 
+- [React Web Interface](/lib/ui-001)
+- The contract used [Smart Contracts](lib/contracts)
+    1 The deployment scripts for local envirnoments, [is here](lib/contracts/scripts)
+    2 The deployment scripts for browser wallet deployment [is here](lib/web-ui/ui-001/src/blockchain)
+ - 
+
+## Description
+
+### Chainlink Integration
+
+Chainlink Automation is used to automate some of the smart contract tasks.
+For the [JoinPool.sol](lib/contracts/policy/JoinPool.sol) contract Chainlink Automation is used to send funds to a authorized pools and to leave a pool when after the proposed time has occured. These functions are [located here](lib/contracts/policy/JoinPool.sol#L).
--- lib/contracts/vault/TreasuryToken.sol
+++ lib/contracts/vault/TreasuryToken.sol
@@ -1,47 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract TreasuryToken is ERC20 {
+contract TreasuryToken is ERC20, Ownable {
     
-    mapping(address => bool) private _owner;
-    address private _rOwner;
+    constructor(
+        string memory name, 
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) Ownable(msg.sender) {
+        // _mint(msg.sender, initialSupply);   
+    }
     
-    constructor(string memory name, string memory symbol ) ERC20(name, symbol) {
-        _owner[msg.sender] = true;
-        _rOwner = msg.sender;
-    }
-
-    modifier auth(){
-       require(_owner[msg.sender]);
-        _;
-    }
-
-    // Remove the treasury owner as an owner of token contract
-    // Make treasury only owner
-    function initTreasury(address newOwner)external auth virtual returns(bool) {
-        require(totalSupply() == 0);
-        _owner[_rOwner] = false;
-        _owner[newOwner] = true;
-        if(_owner[_rOwner]){
-            return false;
-        }
+    // Transfer ownership to the treasury contract
+    function initTreasury(address newOwner) external onlyOwner returns(bool) {
+        require(totalSupply() > 0, "No tokens minted");
+        transferOwnership(newOwner);
         return true;
     }
+
     /// @notice Mint new tokens to an account (onlyOwner)
     /// @param to The address to receive the minted tokens
     /// @param amount The amount of tokens to mint
-    function mintTreasury(address to, uint256 amount) public auth virtual returns(uint256) {
+    function mintTreasury(address to, uint256 amount) public onlyOwner returns(uint256) {
         _mint(to, amount);
         return amount;
     }
-
-    //Add an authorized user
-    function addAuth(address authUser) external auth virtual {
-        _owner[authUser] = true;
-    }
-    
 }
-
